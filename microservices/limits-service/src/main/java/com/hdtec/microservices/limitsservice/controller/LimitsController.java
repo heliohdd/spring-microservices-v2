f7561f3ad74debe3a1189190e4cc110ea94f1fad
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-18 19:52:30 -0300

    Step 03 - Get configuration from application.properties

--- microservices/limits-service/src/main/java/com/hdtec/microservices/limitsservice/controller/LimitsController.java
+++ microservices/limits-service/src/main/java/com/hdtec/microservices/limitsservice/controller/LimitsController.java
@@ -1,19 +1,28 @@
 package com.hdtec.microservices.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hdtec.microservices.limitsservice.bean.Limits;
+import com.hdtec.microservices.limitsservice.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private Configuration configuration;
+	
 	/**
-	 * This Hardcoded Limits service respond with the values in parenthesis 
+	 * Step 03
+	 * Limits service respond with the values in application.properties through Configuration component 
 	 * @return
 	 */
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1,1000);
+		
+		return new Limits(configuration.getMinimum(), 
+				configuration.getMaximum());
+//		return new Limits(1,1000);
 	}
 }
