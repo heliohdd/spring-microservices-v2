6d8dd059e3db02b7a8faedb364cea73c63c616d7
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-22 18:37:39 -0300

    Step 13 - Configure JPA and Initialized Data

--- microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CurrencyExchange.java
+++ microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,11 +2,22 @@ package com.hdtec.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
 
+	@Id
 	private Long id;
+	
+	@Column(name = "currency_from")
 	private String from;
+
+	@Column(name = "currency_to")
 	private String to;
+	
 	private BigDecimal conversionMultiple;
 	private String environment;
 
