be4e491af0891f0cd25c5af78eb4538deea3eb53
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-23 16:37:20 -0300

    Step 18 - Using Feign REST Client for Service Invocation

--- microservices/currency-conversion-service/src/main/java/com/hdtec/microservices/currencyconversionservice/CurrencyConversionController.java
+++ microservices/currency-conversion-service/src/main/java/com/hdtec/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.hdtec.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
@@ -34,5 +38,22 @@ public class CurrencyConversionController {
 				currencyConversion.getConversionMultiple(), 
 				quantity.multiply(currencyConversion.getConversionMultiple()), 
 				currencyConversion.getEnvironment()+ " " + "rest template");
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+				
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment() + " " + "feign");
 	}
 }
---
+++ microservices/currency-conversion-service/src/main/java/com/hdtec/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.hdtec.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable("from") String from,
+			@PathVariable("to") String to);
+	
+}
