63ae2b082f03c5f0b46911d18b24375547f82c75
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-23 11:47:51 -0300

    Step 14 - Create a JPA Repository

--- microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.hdtec.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
 	@Autowired
+	private CurrencyExchangeRepository repository;
+
+	@Autowired
 	private Environment environment;
-	
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(
-			@PathVariable String from,
+			@PathVariable String from, 
 			@PathVariable String to) {
-				CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, 
-						BigDecimal.valueOf(50));
-				String port = environment.getProperty("local.server.port");
-				currencyExchange.setEnvironment(port);
-				return currencyExchange;
+		
+		CurrencyExchange currencyExchange 
+				= repository.findByFromAndTo(from, to);
+		
+		if(currencyExchange ==null) {
+			throw new RuntimeException
+				("Unable to Find data for " + from + " to " + to);
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		
+		currencyExchange.setEnvironment(port);
+
+		return currencyExchange;
 	}
 }
