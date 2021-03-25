d47c1d52b27fd1a920363a52e0ed4996f18add82
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-25 11:09:21 -0300

    Step 27 - Resilience4j Retry and Fallback Methods

--- microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,31 @@
 package com.hdtec.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
 
+	private Logger logger = 
+			LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
-		return "Sample API";
+		logger.info("Sample api call received");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", 
+				String.class);
+		return forEntity.getBody();
+//		return "Sample API";
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "Fallback-response execution exchange!!!";
 	}
 }
