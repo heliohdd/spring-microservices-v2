0cd67632806f6993143dc457095fa2848d8fda5a
Helio Dourado <heliohdd@yahoo.com.br>
2021-03-25 14:17:02 -0300

    Step 28 - Resilience4j Circuit Breaker

--- microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ microservices/currency-exchange-service/src/main/java/com/hdtec/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class CircuitBreakerController {
@@ -16,7 +16,8 @@ public class CircuitBreakerController {
 			LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
 	public String sampleApi() {
 		logger.info("Sample api call received");
 		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", 
